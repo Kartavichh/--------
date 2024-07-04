6d3097256b03a4425415479c8473f59ff299335d
Kartavich <sukhanov.krut@mail.ru>
2024-07-04 22:06:34 +0300

    Add React + node.js

---
+++ Сайт по практике/weather/src/App.js
@@ -0,0 +1,17 @@
+import logo from './favicon.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Сайт находится в разработке, приносим свои извинения за неудобства
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
